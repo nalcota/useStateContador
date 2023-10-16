6d04fbd32dc11f07c27b19f29cfc45fd0699ade3
nalcota <ni.alcota@duoucuc.cl>
2023-10-16 19:28:03 -0300

    Subiendo proyecto

---
+++ src/CounterApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const counterIncrement = () => setCounter(c => c + 1);
+    
+
+    return <>
+        <h2>El valor del contador es { counter }</h2>
+        <button onClick={ counterIncrement } >incrementar contador +1</button>
+    </>
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CounterApp } from './CounterApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CounterApp value={ 7 } />
+  </React.StrictMode>
+)
